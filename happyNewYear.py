6db3c6279269840ebad494009c07400801c18b5a
Mike Poirier <mpoirier@MVP.local>
2020-01-06 13:54:29 +0400

    happy new year

---
+++ happyNewYear.py
@@ -0,0 +1,17 @@
+# Mike Poirier (2019-01-06)
+# Happy New Year display for command prompt
+# Use Control + Z (OSX) to suspend / exit command prompt
+
+import time
+
+count = 0
+year = 2020
+
+while count < 2020:
+     print("Happy")
+     time.sleep(1)
+     print("\tNew")
+     time.sleep(1)
+     print("\t\tYear!")
+     time.sleep(1)
+     count = count + 1
