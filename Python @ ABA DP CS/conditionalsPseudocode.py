dc8b3a4be5ac2a0d2f1356ea358984e2f2062c62
Mike Poirier <mpoirier@gmail.com>
2020-09-29 09:41:39 +0400

    Python @ ABA

---
+++ Python @ ABA DP CS/conditionalsPseudocode.py
@@ -0,0 +1,30 @@
+"""
+NAME: Mr. Poirier
+DATE: 2020-09-29
+PURPOSE: Conditionals, Pseudocode, Flowcharts
+"""
+
+userNum = int(input("A number, please: "))
+
+if userNum > 0:
+    print("It's positive!")
+elif userNum < 0:
+    print("It's negative!")
+else:
+    print("It must be zero!")
+
+"""
+IB PSEUDOCODE NOTATION:
+-----------------------
+
+USERNUM = input "A number, please: "
+
+if USERNUM > 0 then
+    output "It's positive!"
+else if USERNUM < 0 then
+    output "It's negative!"
+else
+    output "It must be zero!"
+end if
+    
+"""
